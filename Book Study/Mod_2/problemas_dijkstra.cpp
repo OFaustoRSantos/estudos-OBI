2e9f2f36f555ccfe4cb9c80123861d68d944fde0
Fausto Santos <faustosantos.work@gmail.com>
2026-09-07 13:06:31 -0300

    corrigindo erros basicos no dijkstra criado

--- Book Study/Mod_2/problemas_dijkstra.cpp
+++ Book Study/Mod_2/problemas_dijkstra.cpp
@@ -2,22 +2,22 @@
 using namespace std;
 using ll = long long;
 
-struct ponto{int destino; int custo;
+struct ponto{int destino; ll custo;
     bool operator<(const ponto &other) const{
         if(custo == other.custo){
-            return destino < destino;
+            return destino < other.destino;
         }
         return custo < other.custo;
     }
     bool operator>(const ponto &other) const{
         if(custo == other.custo){
-            return destino > destino;
+            return destino > other.destino;
         }
         return custo > other.custo;
     }
 };
 
-const int INF = 9e10;
+const ll INF = 9e10;
 
 int main(){
     ios::sync_with_stdio(false);
@@ -43,23 +43,23 @@ int main(){
 
     ponto ponto_ini; ponto_ini.custo =0; ponto_ini.destino=0; 
 
-    vector<int> dist(N, INF);
+    vector<ll> dist(N, INF);
     dist[0] = 0;
     pq.push(ponto_ini);
 
     while(!pq.empty()){
         auto ponto_at = pq.top(); pq.pop();
 
-        if(ponto_at.custo != dist[N])
+        if(ponto_at.custo != dist[ponto_at.destino])
             continue;
 
         for(auto e : mat_adj[ponto_at.destino]){
             ponto next_ponto; 
             next_ponto.custo = e.custo + ponto_at.custo; 
-            if(next_ponto.custo > dist[e.destino]) continue;
+            if(next_ponto.custo >= dist[e.destino]) continue;
 
             next_ponto.destino = e.destino;
-
+            dist[next_ponto.destino] = next_ponto.custo;
             pq.push(next_ponto);
         }
         
